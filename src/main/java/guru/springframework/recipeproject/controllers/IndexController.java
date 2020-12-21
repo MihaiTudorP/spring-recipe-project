897b04e91a10893e0d7fec7e1633bb7be520458e
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-12-21 14:45:34 +0200

    Set up repository query methods. Closes #15.

--- src/main/java/guru/springframework/recipeproject/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeproject/controllers/IndexController.java
@@ -1,13 +1,31 @@
 package guru.springframework.recipeproject.controllers;
 
+import guru.springframework.recipeproject.domain.Category;
+import guru.springframework.recipeproject.domain.UnitOfMeasure;
+import guru.springframework.recipeproject.repositories.CategoryRepository;
+import guru.springframework.recipeproject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println(String.format("Category id is: %d", categoryOptional.get().getId()));
+        System.out.println(String.format("Unit of measure id is: %d", unitOfMeasureOptional.get().getId()));
         return "index";
     }
 }
