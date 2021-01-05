1d9a953621b7eeca15d5c386fa6fddd7691ccbe1
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2021-01-05 12:55:21 +0200

    Added sample integration tests. Closes #25.

---
+++ src/test/java/guru/springframework/recipeproject/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.recipeproject.repositories;
+
+import guru.springframework.recipeproject.domain.Category;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CategoryRepositoryTest {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Test
+    public void findByDescription() {
+        final String description = "American";
+        Category category = categoryRepository.findByDescription(description).orElse(null);
+        assertEquals(description, category.getDescription());
+    }
+}
